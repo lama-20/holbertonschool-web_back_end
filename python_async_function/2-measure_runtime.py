cb750bdc89206bec83fe00c0aad2aa89cc3db9e9
Lama <lama72316@gmail.com>
2026-08-29 20:46:15 +0300

    Measure runtime of concurrent coroutines

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Measure the runtime of wait_n."""
+
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Measure the average execution time of wait_n."""
+    start = time.perf_counter()
+    __import__('asyncio').run(wait_n(n, max_delay))
+    end = time.perf_counter()
+
+    return (end - start) / n
