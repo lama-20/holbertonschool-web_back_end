cfa1da1abbd2db70907c113551f91287bcf4b286
Lama <lama72316@gmail.com>
2026-08-29 20:11:11 +0300

    Add type annotations to floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Module for calculating the floor of a float."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of a float."""
+    return math.floor(n)
